e7bd596a29f2c890df332b65c2db7cfa1a9617f7
Philip Karasev <keroone17@gmail.com>
2016-12-17 22:35:34 +0200

    Refactoring
    List of changes: took html+css plate of app by @AlexPurhalo, rewrited script.js in functions, added comments

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-function req() {	
+// counter of completed/skipped quesions
+let totalCount = 1;
+
+let res;
+
+/*
+ * function that make request to http://jservice.io/api/random
+ * @returns {array} res - array with one object of question params
+*/
+function req() {
+	let res;
 	const xhr = new XMLHttpRequest();
 
 	xhr.open('GET', 'http://jservice.io/api/random', false);
@@ -9,29 +19,70 @@ function req() {
 	  // обработать ошибку
 	  console.log( xhr.status + ': ' + xhr.statusText ); // пример вывода: 404: Not Found
 	} else {
-	  const res = JSON.parse(xhr.responseText);
-	  const question = res[0].question;
-	  const answer = res[0].answer;
-	  const id = res[0].id;
-	  const category = res[0].category.title;
-
-
-	  document.getElementsByClassName("question")[0].innerHTML = question;
-	  document.getElementsByClassName("id")[0].innerHTML = 'QUESTION #' + id;
-	  console.log(id + ': ' + answer);
-	  document.getElementsByClassName("category")[0].innerHTML = 'Category: ' + category;
+	  res = JSON.parse(xhr.responseText);
 	}
+
+	return res;
 }
 
-req();
+/*
+ * function that call req(); then check more than 1 word is answer
+	 then call push();
+*/
+function render() {
+	let midRes;
+	let resArr = [];
 
-let totalCount = 0;
+	// check if answer has more than 1 word - skip it
+	while (!resArr.length || resArr.length !== 1) {
+		midRes = req();
+		resArr = midRes[0].answer.split(' ');
+	}
 
-function skip() {
+	// get params from response
+	const question = midRes[0].question;
+	const answer = midRes[0].answer;
+	const id = midRes[0].id;
+	const category = midRes[0].category.title;
+
+	// push question with next params
+	push(question, id, answer, category);
+}
+
+/*
+ * function that push params into html page
+ * @param {string} question - parsed question from http://jservice.io/api/random
+ * @param {string} id - parsed id of question from http://jservice.io/api/random
+ * @param {string} answer - parsed answer for question from http://jservice.io/api/random
+ * @param {string} category - parsed category of question from http://jservice.io/api/random
+*/
+function push(question, id, answer, category) {
+	document.getElementsByClassName("question")[0].innerHTML = question;
+	document.getElementsByClassName("id")[0].innerHTML = 'QUESTION #' + id;
+	console.log(id + ': ' + answer);
+	document.getElementsByClassName("category")[0].innerHTML = 'Category: ' + category;
+}
+
+render();
+
+/*
+ * function for get next question
+*/
+function next() {
 	document.getElementsByClassName("total")[0].innerHTML = 'Total questions: ' + totalCount;
 	++totalCount;
-	req();
+	render();
 }
+
+/*
+ * function for skip button
+*/
+function skip() {
+	next();
+}
+
+/*
+ * example of response from http://jservice.io/api/random
 
 const example = [{"id":33875,
 "answer":"The Green Berets",
@@ -49,4 +100,4 @@ const example = [{"id":33875,
 	"created_at":"2014-02-11T23:05:55.198Z",
 	"updated_at":"2014-02-11T23:05:55.198Z",
 	"clues_count":16}
-}]
+}]*/
